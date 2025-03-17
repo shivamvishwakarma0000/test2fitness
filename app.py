0a507f7919a94c98680841b5bca6fc70f5566eab
shivamvishwakarma0000 <145027363+shivamvishwakarma0000@users.noreply.github.com>
2025-03-17 23:21:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 from flask import Flask, render_template, request,redirect, url_for
+import os
 app=Flask(__name__)
 
 @app.route("/",methods=["GET"])
@@ -31,5 +32,6 @@ def register():
 
 
 
-if __name__=="__main__":
-    app.run(debug=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
